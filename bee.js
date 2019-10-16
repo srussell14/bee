b1bf7564c1ed0edf2f2fc401924eb2d250b618d4
srussell14 <54918910+srussell14@users.noreply.github.com>
2019-10-15 23:43:47 -0700

    Update 4

--- bee.js
+++ bee.js
@@ -7,11 +7,14 @@ var backgroundLogo;
 
 var blimp;
 var blimpX = 790;
-var blimpY = 80;
+var blimpY = 125;
 var blimpDirection = -1;
 
 var bee1Xarray = [];
 var bee1Yarray = [];
+
+var bee2Xarray = [];
+var bee2Yarray = [];
 
 function preload() {
   backgroundLogo = loadImage("assets/background_logo.png");
@@ -26,13 +29,27 @@ function setup() {
   for(var i=0;i<1;i++){
   bee1Xarray[i]=random(150,150)}
   for(var i=0;i<1;i++){
-  bee1Yarray[i]=random(400,400)}
+  bee1Yarray[i]=random(425,425)}
+
+  for(var j=0;j<3;j++){
+  bee2Xarray[j]=random(200,200)}
+  for(var j=0;j<3;j++){
+  bee2Yarray[j]=random(450,450)}
+
 }
 function draw() {
     displayThankYouPage();
 }
 function displayThankYouPage() {
   image(backgroundLogo, width / 2, height / 2, width, height);
+
+  for(var j=0;j<3;j++){
+  image(bee, bee2Xarray[j],bee2Yarray[j], width/15, height/23);
+  bee2Xarray[j]+= random(-2,2);
+  bee2Yarray[j]-= random(-2,2);
+
+}
+
   for(var i=0;i<1;i++){
   image(bee, bee1Xarray[i],bee1Yarray[i], width/9, height/16);
   bee1Xarray[i]+= random(-2,2);
@@ -44,8 +61,6 @@ function displayThankYouPage() {
     blimpDirection = -350
     blimpX = 790;
     blimpDirection = -1;
-
-}
-
+  }
 
 }
